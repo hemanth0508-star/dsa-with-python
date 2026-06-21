8cc19eb80aca97fe71eecbc5c2ef2fbdab95fb10
unknown <ch.hemanth0508.@gmail.com>
2026-06-21 19:07:10 +0530

    added code

---
+++ Arrays/maximum_subarray.py
@@ -0,0 +1,20 @@
+'''
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+'''
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+sum=nums[0]
+max=nums[0]
+start=0
+end=0
+temp=0
+for i in range(1,len(nums)):
+    if nums[i]>sum+nums[i]:
+        sum=nums[i]
+        temp=i
+    else:
+        sum=nums[i]+sum
+    if max<sum:
+        max=sum
+        start=temp
+        end=i
+print(max,nums[start:end+1])
