1c2b97d91ad6c081177750915a46e0b563a60dc6
unknown <ch.hemanth0508.@gmail.com>
2026-06-21 13:01:41 +0530

    updated the code

--- strings/valid_palindrome.py
+++ strings/valid_palindrome.py
@@ -1,3 +1,12 @@
+s="A man, a plan, a canal: Panama"
+
+res=[i.lower() for i in s if i.isalnum()]
+rev=res[::-1]
+if rev==res:
+    print("given string is a valid palindrome")
+else:
+    print("given string is not a valid palindrome")
+    
 '''
 Example 1:
 
@@ -16,14 +25,3 @@ Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 '''
-
-
-
-s="A man, a plan, a canal: Panama"
-
-res=[i.lower() for i in s if i.isalnum()]
-rev=res[::-1]
-if rev==res:
-    print("given string is a valid palindrome")
-else:
-    print("given string is not a valid palindrome")
