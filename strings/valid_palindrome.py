98625317dfb8e79445c805f003969f55e7673fd9
unknown <ch.hemanth0508.@gmail.com>
2026-06-21 12:59:58 +0530

    updated the code

--- strings/valid_palindrome.py
+++ strings/valid_palindrome.py
@@ -1,3 +1,24 @@
+'''
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+'''
+
+
+
 s="A man, a plan, a canal: Panama"
 
 res=[i.lower() for i in s if i.isalnum()]
