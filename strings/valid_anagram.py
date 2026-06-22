5d5d2b2562912422e267283500f309d08bbf5891
unknown <ch.hemanth0508.@gmail.com>
2026-06-22 21:38:47 +0530

    updated code

---
+++ strings/valid_anagram.py
@@ -0,0 +1,41 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+'''
+
+s = "anagram"
+t = "nagaram"
+
+s=sorted(s)
+t=sorted(t)
+if s==t:
+    print("True")
+else:
+    print("False")
+    
+#another code or another logic
+'''
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    freq = {}
+
+    for ch in s:
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+
+    for ch in t:
+        if ch not in freq:
+            return False
+        freq[ch] -= 1
+        if freq[ch] < 0:
+            return False
+
+    return True
+
+# Example
+print(isAnagram("anagram", "nagaram"))  # True
+print(isAnagram("rat", "car"))          # False
+'''
