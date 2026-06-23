7cc097bceb61514f2519edce03aa113f3beb0e6a
unknown <ch.hemanth0508.@gmail.com>
2026-06-23 19:49:01 +0530

    added code to the file

---
+++ strings/group_anagrams.py
@@ -0,0 +1,17 @@
+'''
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+'''
+strs=["eat","tea","tan","ate","nat","bat"]
+
+hash_map={}
+for string in strs:
+    sorted_string=''.join(sorted(string))
+    if sorted_string in hash_map:
+        hash_map[sorted_string].append(string)
+    else:
+        hash_map[sorted_string]=[string]
+print(hash_map)
+print(hash_map.values())        
