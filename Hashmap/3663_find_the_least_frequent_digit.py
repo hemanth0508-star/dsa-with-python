fb80ab0c48ebe53ae8f01c7c9001afbbe8dd58e0
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-25 23:01:54 +0530

    Update 3663_find_the_least_frequent_digit.py

--- Hashmap/3663_find_the_least_frequent_digit.py
+++ Hashmap/3663_find_the_least_frequent_digit.py
@@ -1,1 +1,18 @@
-
+'''Given an integer n, find the digit that occurs least frequently in its decimal representation. If multiple digits have the same frequency, choose the smallest digit.
+Return the chosen digit as an integer.
+The frequency of a digit x is the number of times it appears in the decimal representation of n.'''
+n = 1553322
+freq={}
+while n:
+  rem=n%10
+  if rem in freq:
+    freq[rem]+=1
+  else:
+    freq[rem]=1
+  n=n//10
+m=min(freq.values())
+l_f={}
+for key in freq:
+  if freq[key]==m:
+    l_f[key]=1
+print( min(l_f.keys()))
