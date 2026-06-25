0f73b73e91709b3728017e4ccdfcd13087c5e0c2
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-25 21:55:25 +0530

    Update 242_valid_anagram.py

--- strings/242_valid_anagram.py
+++ strings/242_valid_anagram.py
@@ -12,30 +12,4 @@ if s==t:
 else:
     print("False")
     
-#another code or another logic
-'''
-def isAnagram(s, t):
-    if len(s) != len(t):
-        return False
 
-    freq = {}
-
-    for ch in s:
-        if ch in freq:
-            freq[ch] += 1
-        else:
-            freq[ch] = 1
-
-    for ch in t:
-        if ch not in freq:
-            return False
-        freq[ch] -= 1
-        if freq[ch] < 0:
-            return False
-
-    return True
-
-# Example
-print(isAnagram("anagram", "nagaram"))  # True
-print(isAnagram("rat", "car"))          # False
-'''
