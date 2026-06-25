4c60aab08e72a203248689d0a7391a5615ec7c3e
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-25 22:46:12 +0530

    Update 3005_elements_with_maximum_frequence.py

--- Hashmap/3005_elements_with_maximum_frequence.py
+++ Hashmap/3005_elements_with_maximum_frequence.py
@@ -1,1 +1,20 @@
+'''
+You are given an array nums consisting of positive integers.
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+The frequency of an element is the number of occurrences of that element in the array.
+'''
+nums = [1,2,2,3,1,4]
+
+freq={}
+for i in range(len(nums)):
+  freq[nums[i]]=freq.get(nums[i],0)+1
+max=0
+for key in freq:
+  if freq[key]>max:
+    max=freq[key]
+ans=0
+for key in freq:
+  if freq[key]==max:
+    ans+=freq[key]
+print( ans)
 
