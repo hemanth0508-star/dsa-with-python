d98ef2dd51b2e774dd9d49768fb462219a329e15
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-25 23:23:28 +0530

    Update 345_reverse_vowels_of_a_string.py

--- strings/345_reverse_vowels_of_a_string.py
+++ strings/345_reverse_vowels_of_a_string.py
@@ -3,23 +3,21 @@ Given a string s, reverse only all the vowels in the string and return it.
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 '''
 
-s="leetcode"
-vowels="AEIOUaeiou"
-i=0
-j=len(s)-1
-s=list(s)
-while i<j:
-    if s[i] not in vowels:
-        i+=1
-    if s[j] not in vowels:
-        j-=1
-    if s[i] in vowels and s[j] in vowels:
-        if i<j:
-            temp=s[i]   
-            s[i]=s[j]
-            s[j]=temp
-            i+=1
-            j-=1
-s=''.join(s)
-print(s)
-        
+s = "leetcode"
+vowels = "AEIOUaeiou"
+v = ""
+for ch in s:
+    if ch in vowels:
+        v = ch + v      
+
+ans = ""
+j = 0
+
+for ch in s:
+    if ch in vowels:
+        ans += v[j]
+        j += 1
+    else:
+        ans += ch
+
+print(ans)
