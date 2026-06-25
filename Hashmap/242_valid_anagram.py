d589a28bbcb2f1cd5db822db13c5cd6d0bc2701c
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-25 21:58:38 +0530

    Refactor isAnagram function for clarity
    Refactored isAnagram function to improve readability.

--- Hashmap/242_valid_anagram.py
+++ Hashmap/242_valid_anagram.py
@@ -1,23 +1,24 @@
-def isAnagram(s, t):
-    if len(s) != len(t):
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+'''
+s="anagram"
+t="nagaram"
+if len(s) != len(t):
+    return False
+freq = {}
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+for ch in t:
+    if ch not in freq:
         return False
-
-    freq = {}
-
-    for ch in s:
-        if ch in freq:
-            freq[ch] += 1
-        else:
-            freq[ch] = 1
-
-    for ch in t:
-        if ch not in freq:
-            return False
-        freq[ch] -= 1
-        if freq[ch] < 0:
-            return False
-
-    return True
+    freq[ch] -= 1
+    if freq[ch] < 0:
+        return False
+return True
 '''
 # Example
 print(isAnagram("anagram", "nagaram"))  # True
