ec46d2ad9325510b94bdfefdf1c5059a39963f6f
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-26 11:29:55 +0530

    Update 628_maximum_product_of_three_numbers.py

--- math/628_maximum_product_of_three_numbers.py
+++ math/628_maximum_product_of_three_numbers.py
@@ -1,1 +1,8 @@
+'''
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+'''
 
+nums=[1,2,3,4,2,5,7,8,2,3,6]
+
+nums.sort()
+print(max(nums[0]*nums[1]*nums[-1],nums[-1]*nums[-2]*nums[-3]))
