13921d4de43aecff59e11a098c3bee80e17e2771
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-26 11:23:07 +0530

    Update 202_happy_number.py

--- math/202_happy_number.py
+++ math/202_happy_number.py
@@ -1,1 +1,21 @@
-
+'''Write an algorithm to determine if a number n is happy.
+A happy number is a number defined by the following process:
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+ '''
+n=19
+visit=set()
+while n!=1 and n not in visit:
+  visit.add(n)
+  total=0
+  while n:
+    digit=n%10
+    total+=digit**2
+    n//=10
+  n=total
+if n==1:
+  print("True")
+else:
+  print("Fasle")
