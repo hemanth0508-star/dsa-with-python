03fa8f0f9c22e5798d2f6b7acaca9b9bf5c80b89
challa hemanth <ch.hemanth0508@gmail.com>
2026-06-26 11:06:45 +0530

    Update 009_palindrome_number.py

--- math/009_palindrome_number.py
+++ math/009_palindrome_number.py
@@ -1,1 +1,15 @@
-
+'''
+Given an integer x, return true if x is a palindrome, and false otherwise.
+'''
+n=1221
+temp=n
+i=0
+while n:
+  rem=n%10
+  i=i*10+rem
+  n//=10
+if i==temp:
+  print("True")
+else:
+  print("False")
+  
