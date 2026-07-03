60158786827028783a467c72be76a39a22eab69e
challa hemanth <ch.hemanth0508@gmail.com>
2026-07-03 22:33:14 +0530

    Update docstring with input/output examples
    Refactor code to include input and output examples in the docstring.

--- binary search/1011_capacity_to_ship_packages_within_d_days.py
+++ binary search/1011_capacity_to_ship_packages_within_d_days.py
@@ -1,6 +1,8 @@
 '''A conveyor belt has packages that must be shipped from one port to another within days days.
 The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
 Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
 '''
 weights = [1,2,3,4,5,6,7,8,9,10]
 days = 5
