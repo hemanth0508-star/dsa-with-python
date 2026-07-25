d29257ef0629947f3421fa968baf5de5f9362ad0
challa hemanth <ch.hemanth0508@gmail.com>
2026-07-25 20:39:43 +0530

    Add countPrimes method to count prime numbers
    Implement Sieve of Eratosthenes to count primes.

--- math/204_count_primes.py
+++ math/204_count_primes.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<2:
+            return 0
+        prime=[True]*n
+        prime[0],prime[1]=False,False
+        p=2
+        ans=0
+        while p<n:
+            if prime[p]==True:
+                ans+=1
+                for i in range(p*p,n,p):
+                    prime[i]=False
+            p+=1
+        return ans
