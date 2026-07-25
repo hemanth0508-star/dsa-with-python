53271eeb3e44b922d74a407ab9ccc7bba3f1a406
challa hemanth <ch.hemanth0508@gmail.com>
2026-07-25 20:40:49 +0530

    Add example input and output for count_primes function
    Added example input and output comments for clarity.

--- math/204_count_primes.py
+++ math/204_count_primes.py
@@ -13,3 +13,7 @@ class Solution:
                     prime[i]=False
             p+=1
         return ans
+
+#Input: n = 10
+#Output: 4
+#Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
