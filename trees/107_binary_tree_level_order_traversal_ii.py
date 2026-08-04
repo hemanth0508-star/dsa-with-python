d45a8d834f1b6ad5aa5d789a7c709c0a46d5e18f
challa hemanth <ch.hemanth0508@gmail.com>
2026-08-04 15:35:46 +0530

    Implement level order traversal from bottom

--- trees/107_binary_tree_level_order_traversal_ii.py
+++ trees/107_binary_tree_level_order_traversal_ii.py
@@ -1,1 +1,24 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q=[root]
+        res=[]
+        while q:
+            n=len(q)
+            t=[]
+            for i in range(n):
+                node=q.pop(0)
+                t.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(t)
+        return res[::-1]
