2f10b51e3b47b7190ec56c33a536b09e49fa37fc
challa hemanth <ch.hemanth0508@gmail.com>
2026-08-12 19:20:49 +0530

    Add docstring for preorder traversal function
    Added a docstring to explain the function's purpose.

--- trees/144_binary_tree_preorder_traversal.py
+++ trees/144_binary_tree_preorder_traversal.py
@@ -1,3 +1,5 @@
+#Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
