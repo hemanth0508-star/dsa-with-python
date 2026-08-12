8bff720bb4eb28f898608e1ed30b6d20d6ca6e88
challa hemanth <ch.hemanth0508@gmail.com>
2026-08-12 19:21:29 +0530

    Add comment for binary tree postorder traversal
    Added a comment to describe the postorder traversal function.

--- trees/145_binary_tree_postorder_traversal.py
+++ trees/145_binary_tree_postorder_traversal.py
@@ -1,3 +1,4 @@
+#Given the root of a binary tree, return the postorder traversal of its nodes' values.
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
