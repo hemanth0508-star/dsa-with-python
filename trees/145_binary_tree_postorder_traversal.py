0860984f66a72533e2da9c4489eef229d841e568
challa hemanth <ch.hemanth0508@gmail.com>
2026-08-12 19:19:04 +0530

    Implement postorder traversal for binary tree

--- trees/145_binary_tree_postorder_traversal.py
+++ trees/145_binary_tree_postorder_traversal.py
@@ -1,1 +1,17 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        def postord(node):
+            if not node:
+                return 
+            postord(node.left)
+            postord(node.right)
+            ans.append(node.val)
+        postord(root)
+        return ans
