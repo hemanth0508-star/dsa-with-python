84064f9d100973e55f77cce1b660c751c12e32b9
challa hemanth <ch.hemanth0508@gmail.com>
2026-08-12 19:20:01 +0530

    Implement preorder traversal for binary tree

--- trees/144_binary_tree_preorder_traversal.py
+++ trees/144_binary_tree_preorder_traversal.py
@@ -1,27 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.next = next
+#         self.left = left
+#         self.right = right
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        l1=list1
-        l2=list2
-        l3=ListNode('h')
-        curr1=l1
-        curr2=l2
-        curr3=l3
-        while curr1 and curr2:
-            if curr1.val<=curr2.val:
-                curr3.next=curr1
-                curr1=curr1.next
-                curr3=curr3.next
-            else:
-                curr3.next=curr2
-                curr2=curr2.next
-                curr3=curr3.next
-        if curr1:
-            curr3.next=curr1
-        if curr2:
-            curr3.next=curr2
-        return l3.next        
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        def pre(node):
+            if not node:
+                return
+            ans.append(node.val)
+            pre(node.left)
+            pre(node.right)
+        pre(root)
+        return ans
